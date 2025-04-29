3f17047d1c241049d16a81acd43b02a7cabf3f2a
niharikacompro <niharikacomprotechnologies.com@gmail.com>
2025-04-29 16:35:39 +0530

    code refactor

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,78 +1,76 @@
 <template>
-       <div
-            v-for="(quiz, id) in quizzes"
-            :key="quiz.id"
-            class=" px-2 relative  bg-white shadow-md rounded-lg p-5 min-h-[160px] flex flex-col justify-between relative"
-          >
-           
-              <!-- 3-dot icon -->
-              <div class="absolute top-3 right-3 z-20">
-                <button
-                  @click="toggleMenu(quiz.id)"
-                  class="text-gray-400 hover:text-gray-600"
-                >
-                  <MoreVertical class="w-5 h-5" />
-                </button>
+  <div
+    v-for="(quiz, id) in quizzes"
+    :key="quiz.id"
+    class="px-2 relative bg-white shadow-md rounded-lg p-5 min-h-[160px] flex flex-col justify-between relative"
+  >
+    <!-- 3-dot icon -->
+    <div class="absolute top-3 right-3 z-20">
+      <button
+        @click="toggleMenu(quiz.id)"
+        class="text-gray-400 hover:text-gray-600"
+      >
+        <MoreVertical class="w-5 h-5" />
+      </button>
 
-                <!-- Dropdown -->
-                <div
-                  v-if="activeMenuId === quiz.id"
-                  class="absolute right-0 mt-2 bg-white border border-gray-200 rounded shadow w-32 z-30"
-                >
-                  <NuxtLink
-                    :to="{
-                      path: '/create-quiz',
-                      query: {
-                        edit: 'true',
-                        id: quiz.id,
-                        title: quiz.title,
-                        description: quiz.description,
-                        questions:JSON.stringify(quiz.questions),
-                      },
-                    }"
-                    class="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
-                    @click="activeMenuId = null"
-                  >
-                    Edit Quiz
-                  </NuxtLink>
-                  <button
-                    class="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
-                    @click="deleteQuiz(quiz.id)"
-                  >
-                    Delete Quiz
-                  </button>
-                </div>
-              </div>
+      <!-- Dropdown -->
+      <div
+        v-if="activeMenuId === quiz.id"
+        class="absolute right-0 mt-2 bg-white border border-gray-200 rounded shadow w-32 z-30"
+      >
+        <NuxtLink
+          :to="{
+            path: '/create-quiz',
+            query: {
+              edit: 'true',
+              id: quiz.id,
+              title: quiz.title,
+              description: quiz.description,
+              questions: JSON.stringify(quiz.questions),
+            },
+          }"
+          class="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"
+          @click="activeMenuId = null"
+        >
+          Edit Quiz
+        </NuxtLink>
+        <button
+          class="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
+          @click="deleteQuiz(quiz.id)"
+        >
+          Delete Quiz
+        </button>
+      </div>
+    </div>
 
-              <!-- Quiz content -->
-              <div>
-                <h3 class="text-lg font-semibold text-gray-800">
-                  {{ quiz.title }}
-                </h3>
-                <p class="text-sm text-gray-600 mt-1">{{ quiz.description }}</p>
-              </div>
+    <!-- Quiz content -->
+    <div>
+      <h3 class="text-lg font-semibold text-gray-800">
+        {{ quiz.title }}
+      </h3>
+      <p class="text-sm text-gray-600 mt-1">{{ quiz.description }}</p>
+    </div>
 
-              <!-- View button -->
-              <div class="mt-4">
-                <NuxtLink
-                  :to="`/preview/${quiz.id}`"
-                  class="inline-block text-sm px-4 py-1.5 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-                >
-                  View
-                </NuxtLink>
-              </div>
-          
-          </div>
+    <!-- View button -->
+    <div class="mt-4">
+      <NuxtLink
+        :to="`/preview/${quiz.id}`"
+        class="inline-block text-sm px-4 py-1.5 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+      >
+        View
+      </NuxtLink>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
-import type { Quiz } from '@/types/quiz';
-import {  MoreVertical } from "lucide-vue-next";
+import type { Quiz } from "@/types/quiz";
+import { MoreVertical } from "lucide-vue-next";
 const emit = defineEmits<{
-  (e: 'update:quizzes', updated: Quiz[]): void;
+  (e: "update:quizzes", updated: Quiz[]): void;
 }>();
 const props = defineProps<{ quizzes: Quiz[] }>();
 const activeMenuId = shallowRef<string | null>(null);
-const toggleMenu = (id:string) => {
+const toggleMenu = (id: string) => {
   activeMenuId.value = activeMenuId.value === id ? null : id;
   console.log("active menu id ", activeMenuId.value);
 };
@@ -83,7 +81,7 @@ const deleteQuiz = (id: string) => {
 
   localStorage.setItem("quizzes", JSON.stringify(updatedQuizzes));
 
-  emit('update:quizzes', updatedQuizzes); // ✅ notify parent
+  emit("update:quizzes", updatedQuizzes); // ✅ notify parent
   activeMenuId.value = null;
 };
 </script>
--- components/QuestionTypeModal.vue
+++ components/QuestionTypeModal.vue
@@ -1,55 +1,52 @@
 <template>
+  <div
+    v-if="show"
+    class="fixed inset-0 bg-black/50 flex justify-center items-center z-[1000]"
+    @click.self="onClose"
+  >
     <div
-      v-if="show"
-      class="fixed inset-0 bg-black/50 flex justify-center items-center z-[1000]"
-      @click.self="onClose"
+      class="bg-white/70 backdrop-blur-[10px] rounded-[15px] p-5 w-[400px] max-w-[90%]"
     >
-      <div class="bg-white/70 backdrop-blur-[10px] rounded-[15px] p-5 w-[400px] max-w-[90%]">
-        <div class="flex justify-between items-center">
-          <h3>Select Question Type</h3>
-          <button
-            class="px-2.5 py-1.5 text-xs font-semibold rounded-lg text-white bg-red-600 bg-opacity-70 transition-all duration-300 ease-in-out shadow-[4px_4px_8px_rgba(163,177,198,0.6),-4px_-4px_8px_rgba(255,255,255,0.7)] hover:-translate-y-0.5 hover:shadow-[6px_6px_10px_rgba(163,177,198,0.7),-6px_-6px_10px_rgba(255,255,255,0.8)] active:translate-y-0 active:shadow-[2px_2px_5px_rgba(163,177,198,0.6),-2px_-2px_5px_rgba(255,255,255,0.7)]"
-            @click="onClose"
-          >
-            <X class="w-4 h-4" />
-          </button>
-        </div>
-  
-        <div class="flex justify-center gap-4 mt-5">
-          <UiButton
-            @click="() => selectType('mcq')"
-            ref="mcqButton"
-            class="bg-white bg-opacity-70 border-none rounded-lg py-2.5 px-5 font-semibold cursor-pointer shadow-md transition-all duration-300 ease-in-out hover:translate-y-[-2px] hover:shadow-lg active:translate-y-0 active:shadow-sm"
-          >
-            Multiple Choice
-          </UiButton>
-          <UiButton
-            @click="() => selectType('input')"
-            class="bg-white bg-opacity-70 border-none rounded-lg py-2.5 px-5 font-semibold cursor-pointer shadow-md transition-all duration-300 ease-in-out hover:translate-y-[-2px] hover:shadow-lg active:translate-y-0 active:shadow-sm"
-          >
-            Text Input
-          </UiButton>
-        </div>
+      <div class="flex justify-between items-center">
+        <h3>Select Question Type</h3>
+        <button
+          class="px-2.5 py-1.5 text-xs font-semibold rounded-lg text-white bg-red-600 bg-opacity-70 transition-all duration-300 ease-in-out shadow-[4px_4px_8px_rgba(163,177,198,0.6),-4px_-4px_8px_rgba(255,255,255,0.7)] hover:-translate-y-0.5 hover:shadow-[6px_6px_10px_rgba(163,177,198,0.7),-6px_-6px_10px_rgba(255,255,255,0.8)] active:translate-y-0 active:shadow-[2px_2px_5px_rgba(163,177,198,0.6),-2px_-2px_5px_rgba(255,255,255,0.7)]"
+          @click="onClose"
+        >
+          <X class="w-4 h-4" />
+        </button>
+      </div>
+
+      <div class="flex justify-center gap-4 mt-5">
+        <UiButton
+          @click="() => selectType('mcq')"
+          ref="mcqButton"
+          class="bg-white bg-opacity-70 border-none rounded-lg py-2.5 px-5 font-semibold cursor-pointer shadow-md transition-all duration-300 ease-in-out hover:translate-y-[-2px] hover:shadow-lg active:translate-y-0 active:shadow-sm"
+        >
+          Multiple Choice
+        </UiButton>
+        <UiButton
+          @click="() => selectType('input')"
+          class="bg-white bg-opacity-70 border-none rounded-lg py-2.5 px-5 font-semibold cursor-pointer shadow-md transition-all duration-300 ease-in-out hover:translate-y-[-2px] hover:shadow-lg active:translate-y-0 active:shadow-sm"
+        >
+          Text Input
+        </UiButton>
       </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { defineProps, defineEmits } from 'vue'
-  import { X } from 'lucide-vue-next' // or wherever your X icon is from
-import type { QuestionType } from '~/types/quiz';
-  const mcqButton = ref<HTMLElement | null>(null);
-  const props = defineProps({
-    show: Boolean,
-  });
-  defineExpose({
-  mcqButton
-})
-
-  
-  const emit = defineEmits(['close', 'select'])
-  
-  const onClose = () => emit('close')
-  const selectType = (type:QuestionType) => emit('select', type)
-  </script>
-  
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+import { X } from "lucide-vue-next"; // or wherever your X icon is from
+import type { QuestionType } from "~/types/quiz";
+const mcqButton = ref<HTMLElement | null>(null);
+const props = defineProps({
+  show: Boolean,
+});
+defineExpose({
+  mcqButton,
+});
+const emit = defineEmits(["close", "select"]);
+const onClose = () => emit("close");
+const selectType = (type: QuestionType) => emit("select", type);
+</script>
